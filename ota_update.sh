f299fbd336275e21ed5a67683359e8fcb9ee9349
Viktor Tsymbalyuk <Viktor.Tsymbalyuk@gmail.com>
2024-07-18 19:35:30 +0300

    full instruction for OTA update

--- ota_update.sh
+++ ota_update.sh
@@ -1,7 +1,31 @@
-# TODO: подумати як робити. бо не має доступу до 192.168.156.116 з desktop
-# як варіант збирати бінарник, заливати на HA, і оновлювати звідти.
-# або custom esphome
+# install arduino-cli
+curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o arduino-cli.tar.gz
+tar -xzf arduino-cli.tar.gz
+sudo mv arduino-cli /usr/local/bin/
+arduino-cli version
 
-# arduino-cli core install home-assistant-integration
-arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 ./bath_fan/bath_fan.ino
-arduino --upload --port 192.168.156.116 --board esp8266:esp8266:nodemcuv2 --auth your_OTA_password src/bath_fan.ino
+# config init
+arduino-cli config init
+vi /home/vic/.arduino15/arduino-cli.yaml
+# vic@HA2:~/ota_update$ cat /home/vic/.arduino15/arduino-cli.yaml
+# board_manager:
+#   additional_urls:
+#     - http://arduino.esp8266.com/stable/package_esp8266com_index.json
+arduino-cli core update-index
+arduino-cli core search esp8266
+arduino-cli core install esp8266:esp8266
+
+# clone and update repo with code
+git clone git@github.com:victron/bath_fan.git
+# add secrets.h
+git pull origin master
+
+# install custom libs
+arduino-cli core install home-assistant-integration
+
+# update via OTA
+git pull origin master
+arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 ./bath_fan/bath_fan.ino --output-dir ./build
+python3 ~/.arduino15/packages/esp8266/hardware/esp8266/3.1.2/tools/espota.py --ip 192.168.156.116 --auth=OTA_password  --file ./build/bath_fan.ino.bin
+
+
