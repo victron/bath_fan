a9b0ce0cd9bbb534ac70773c8c4a8015c2dbaf71
Viktor Tsymbalyuk <Viktor.Tsymbalyuk@gmail.com>
2024-07-22 15:53:15 +0300

    update ota_update.sh

--- ota_update.sh
+++ ota_update.sh
@@ -1,32 +1,45 @@
-# install arduino-cli
-curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o arduino-cli.tar.gz
-tar -xzf arduino-cli.tar.gz
-sudo mv arduino-cli /usr/local/bin/
-arduino-cli version
+#!/bin/bash
+# -------------- install arduino-cli
+# curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o arduino-cli.tar.gz
+# tar -xzf arduino-cli.tar.gz
+# sudo mv arduino-cli /usr/local/bin/
+# arduino-cli version
 
-# config init
-arduino-cli config init
-vi /home/vic/.arduino15/arduino-cli.yaml
-# vic@HA2:~/ota_update$ cat /home/vic/.arduino15/arduino-cli.yaml
-# board_manager:
-#   additional_urls:
-#     - http://arduino.esp8266.com/stable/package_esp8266com_index.json
-arduino-cli core update-index
-arduino-cli core search esp8266
-arduino-cli core install esp8266:esp8266
+# --------------- config init
+# arduino-cli config init
+# vi /home/vic/.arduino15/arduino-cli.yaml
+# # vic@HA2:~/ota_update$ cat /home/vic/.arduino15/arduino-cli.yaml
+# # board_manager:
+# #   additional_urls:
+# #     - http://arduino.esp8266.com/stable/package_esp8266com_index.json
+# arduino-cli core update-index
+# arduino-cli core search esp8266
+# arduino-cli core install esp8266:esp8266
 
-# clone and update repo with code
-git clone git@github.com:victron/bath_fan.git
+# ------------------ clone and update repo with code
+# git clone git@github.com:victron/bath_fan.git
 # add secrets.h
-git pull origin master
+# add secrets.sh
 
-# install custom libs
-arduino-cli core install home-assistant-integration
-arduino-cli lib install "Adafruit BME280 Library"
 
-# update via OTA
-cd bath_fan && git pull && cd ..
-arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 --build-cache-path ./build_cache --output-dir ./build ./bath_fan/bath_fan.ino
-python3 ~/.arduino15/packages/esp8266/hardware/esp8266/3.1.2/tools/espota.py --ip 192.168.156.116 --auth=OTA_password  --file ./build/bath_fan.ino.bin
+# ------------------- install custom libs
+# arduino-cli core install home-assistant-integration
+# arduino-cli lib install "Adafruit BME280 Library"
+
+project_name='bath_fan'
+. ./secrets.sh
+espota='/home/vic/.arduino15/packages/esp8266/hardware/esp8266/3.1.2/tools/espota.py'
+build_cache='~/ota_update/build_cache'
+builds_dir='~/ota_update/build'
+
+git pull
+git log -1
+echo "------- start combile ---------"
+arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 --build-cache-path ${build_cache} --output-dir ${builds_dir} ./${project_name}.ino
+if [ $? -ne 0 ]; then
+  echo "Команда завершилась з помилкою. Завершення скрипта."
+  exit 1
+fi
+python3 ${espota} --ip ${DEVICE_IP} --auth=${OTA_PASSWORD} --file ${builds_dir}/${project_name}.ino.bin
 
 
